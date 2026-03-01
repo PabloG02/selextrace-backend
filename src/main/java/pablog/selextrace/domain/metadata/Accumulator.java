1d5e2d2d576222c6810778edc0233d5f6aed03d1
PabloG02 <devpablogg@gmail.com>
2026-03-01 14:51:38 +0100

    Migrate Accumulator docs to Markdown and separate license

--- src/main/java/pablog/selextrace/domain/metadata/Accumulator.java
+++ src/main/java/pablog/selextrace/domain/metadata/Accumulator.java
@@ -1,31 +1,5 @@
-package pablog.selextrace.domain.metadata;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.io.Serializable;
-
-/**
- *  The {@code Accumulator} class is a data type for computing the running
- *  mean, sample standard deviation, and sample variance of a stream of real
- *  numbers. It provides an example of a mutable data type and a streaming
- *  algorithm.
- *  <p>
- *  This implementation uses a one-pass algorithm that is less susceptible
- *  to floating-point roundoff error than the more straightforward
- *  implementation based on saving the sum of the squares of the numbers.
- *  This technique is due to
- *  <a href = "https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm">B. P. Welford</a>.
- *  Each operation takes constant time in the worst case.
- *  The amount of memory is constant - the data values are not stored.
- *  <p>
- *  For additional documentation, 
- *  see <a href="http://algs4.cs.princeton.edu/12oop">Section 1.2</a> of 
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne. 
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
+/*
+ *  Copyright 2002-2025, Robert Sedgewick and Kevin Wayne.
  *
  *  This file is part of algs4.jar, which accompanies the textbook
  *
@@ -46,11 +20,35 @@ import java.io.Serializable;
  *
  *  You should have received a copy of the GNU General Public License
  *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
+ */
+package pablog.selextrace.domain.metadata;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/// The `Accumulator` class is a data type for computing the running
+/// mean, sample standard deviation, and sample variance of a stream of real
+/// numbers. It provides an example of a mutable data type and a streaming
+/// algorithm.
+///
+/// This implementation uses a one-pass algorithm that is less susceptible
+/// to floating-point roundoff error than the more straightforward
+/// implementation based on saving the sum of the squares of the numbers.
+/// This technique is due to
+/// [B. P. Welford](https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm).
+/// Each operation takes constant time in the worst case.
+/// The amount of memory is constant — the data values are not stored.
+///
+/// For additional documentation, see
+/// [Section 1.2](http://algs4.cs.princeton.edu/12oop) of
+/// *Algorithms, 4th Edition* by Robert Sedgewick and Kevin Wayne.
+///
+/// @author Robert Sedgewick
+/// @author Kevin Wayne
 public class Accumulator implements Serializable {
-    /**
-     * 
-     */
+    @Serial
     private static final long serialVersionUID = 2243113298802820956L;
 
     @JsonProperty
@@ -60,17 +58,13 @@ public class Accumulator implements Serializable {
     @JsonProperty
     private double mu = 0.0; // sample mean
 
-    /**
-     * Initializes an accumulator.
-     */
+    /// Initializes an accumulator.
     public Accumulator() {
     }
 
-    /**
-     * Adds the specified data value to the accumulator.
-     * 
-     * @param x the data value
-     */
+    /// Adds the specified data value to the accumulator.
+    ///
+    /// @param x the data value
     public synchronized void addDataValue(double x) {
         n++;
         double delta = x - mu;
@@ -78,40 +72,24 @@ public class Accumulator implements Serializable {
         sum += (double) (n - 1) / n * delta * delta;
     }
 
-    /**
-     * Returns the mean of the data values.
-     * 
-     * @return the mean of the data values
-     */
+    /// Returns the sample mean of all added data values.
     public double mean() {
         return mu;
     }
 
-    /**
-     * Returns the sample variance of the data values.
-     * 
-     * @return the sample variance of the data values
-     */
+    /// Returns the sample variance, or `NaN` if fewer than two data values have been added.
     public double var() {
         if (n <= 1)
             return Double.NaN;
         return sum / (n - 1);
     }
 
-    /**
-     * Returns the sample standard deviation of the data values.
-     * 
-     * @return the sample standard deviation of the data values
-     */
+    /// Returns the sample standard deviation, or `NaN` if fewer than two data values have been added.
     public double stddev() {
         return Math.sqrt(this.var());
     }
 
-    /**
-     * Returns the number of data values.
-     * 
-     * @return the number of data values
-     */
+    /// Returns the number of data values added so far.
     public int count() {
         return n;
     }
