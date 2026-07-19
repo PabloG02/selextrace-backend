ad07cea6be5f59ff0f2b4a3769b85f63a81497ce
PabloG02 <devpablogg@gmail.com>
2026-07-19 10:49:32 +0200

    Make Google OAuth configuration optional

--- src/main/java/pablog/selextrace/config/OAuth2ClientConfig.java
+++ src/main/java/pablog/selextrace/config/OAuth2ClientConfig.java
@@ -1,6 +1,6 @@
 package pablog.selextrace.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
@@ -11,8 +11,8 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 public class OAuth2ClientConfig {
 
     @Bean
-    @ConditionalOnProperty("selextrace.security.google-client-id")
-    @ConditionalOnProperty("selextrace.security.google-client-secret")
+    @ConditionalOnExpression("'${selextrace.security.google-client-id:}' != '' "
+            + "&& '${selextrace.security.google-client-secret:}' != ''")
     ClientRegistrationRepository clientRegistrationRepository(SecurityProperties securityProperties) {
         return new InMemoryClientRegistrationRepository(
                 CommonOAuth2Provider.GOOGLE
